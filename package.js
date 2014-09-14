4ccd8692d5d15a673dde12f4f88d0be741a6d158
Mitar <mitar.git@tnode.com>
2014-09-14 01:24:16 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,26 +1,10 @@
 Package.describe({
   summary: "URL normalization utils",
   version: '0.4.0-1',
-  name: 'peerlibrary:url-utils',
+  name: 'mrt:url-utils',
   git: 'https://github.com/peerlibrary/meteor-url-utils.git'
 });
 
-Npm.depends({
-  'node-url-utils': '0.4.0'
-});
-
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-
-  api.export('UrlUtils');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:url-utils', 'tinytest', 'test-helpers'], 'server');
-
-  api.add_files('tests.js', 'server');
+  api.imply('peerlibrary:url-utils@0.4.0-1');
 });
